e1036654b759bc6ea98c5b8691292e6f5e5e4af2
lunashimca-crypto <luna.shim.ca@gmail.com>
2026-07-03 21:37:59 +0900

    First commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CIEE Korea Food Journey',
+        short_name: 'Food Journey',
+        description: '7-day Korean food logging experience',
+        theme_color: '#8A3F26',
+        background_color: '#F4E6D8',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+        ],
+      },
+    }),
+  ],
+})
